a786c5e8241d975cb356c728a925428dfb261838
Ruangguru <rais.yashina@ruangguru.com>
2023-12-02 14:36:29 +0700

    change port

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import app from './express/index.js'
 
-app.listen(80, function () {
+const port = process.env.PORT || 8000
+
+app.listen(port, function () {
     console.log("Server start on Port 80");
 })
